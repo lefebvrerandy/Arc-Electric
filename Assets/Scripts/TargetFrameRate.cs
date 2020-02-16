16857018be56a44673ae76d6bc65a345450f20e1
KyleRoger <kyleroger@hotmail.com>
2020-02-16 16:26:51 -0500

    Editing files. .Working on plane point clous

--- Assets/Scripts/TargetFrameRate.cs
+++ Assets/Scripts/TargetFrameRate.cs
@@ -7,7 +7,7 @@ public class TargetFrameRate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 300;
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
