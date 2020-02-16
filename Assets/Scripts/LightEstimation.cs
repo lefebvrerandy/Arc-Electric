2738eaf11f00fa8530460d03e47bfa4aa04881b1
DigitalCitizen110625 <bkarner5307@conestogac.on.ca>
2020-02-16 17:16:59 -0500

    Added comments, references, etc. to some our scripts

--- Assets/Scripts/LightEstimation.cs
+++ Assets/Scripts/LightEstimation.cs
@@ -1,40 +1,35 @@
-﻿using UnityEngine;
+﻿/*
+*  FILE          : LightEstimation.cs
+*  PROJECT       : PROG 3220 - Systems Project
+*  PROGRAMMER    : Randy Lefebvre, Bence Karner, Lucas Roes, Kyle Horsley
+*  DESCRIPTION   : This file contains the LightEstimation class, which is used to control the built in 
+*                  AR light estimation system in both iOS, and Android devices
+*  REFERENCE     : Script format was copied and altered from the following sources,
+*   Unity. (2020). AR Foundation Samples. Retrieved on February 3, 2020, from 
+*   https://github.com/Unity-Technologies/arfoundation-samples
+*/
+
+
+using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.XR.ARFoundation;
 
-/// <summary>
-/// A component that can be used to access the most
-/// recently received light estimation information
-/// for the physical environment as observed by an
-/// AR device.
-/// </summary>
+
+/*
+*   NAME    : LightEstimation
+*   PURPOSE : Uses the recently received light estimation information for the physical environment
+*             as observed by an AR device, in order to adjust the AR shadows, and surface visuals
+*/
 [RequireComponent(typeof(Light))]
 public class LightEstimation : MonoBehaviour
 {
+
     [SerializeField]
     [Tooltip("The ARCameraManager which will produce frame events containing light estimation information")]
     ARCameraManager m_CameraManager;
 
-    //Reference to the light component in the directional light object
-    Light m_Light;
-
     /// <summary>
-    /// The estimated direction of the main light of the physical environment, if available.
-    /// </summary>
-    public Vector3? mainLightDirection { get; private set; }
-
-    /// <summary>
-    /// The estimated color of the main light of the physical environment, if available.
-    /// </summary>
-    public Color? mainLightColor { get; private set; }
-
-    /// <summary>
-    /// The estimated spherical harmonics coefficients of the physical environment, if available.
-    /// </summary>
-    public SphericalHarmonicsL2? sphericalHarmonics { get; private set; }
-    
-    /// <summary>
-    /// Get or set the <c>ARCameraManager</c>.
+    /// Get or set the ARCameraManager
     /// </summary>
     public ARCameraManager cameraManager
     {
@@ -53,13 +48,50 @@ public class LightEstimation : MonoBehaviour
         }
     }
 
-    //Get a reference to the light component from the directional light object
+
+    /// <summary>
+    /// Reference to the light component in the directional light object
+    /// </summary>
+    Light m_Light;
+
+    #region LightProperties
+
+    /// <summary>
+    /// The estimated direction of the main light of the physical environment, if available.
+    /// </summary>
+    public Vector3? mainLightDirection { get; private set; }
+
+    /// <summary>
+    /// The estimated color of the main light of the physical environment, if available.
+    /// </summary>
+    public Color? mainLightColor { get; private set; }
+
+    /// <summary>
+    /// The estimated spherical harmonics coefficients of the physical environment, if available.
+    /// </summary>
+    public SphericalHarmonicsL2? sphericalHarmonics { get; private set; }
+
+    #endregion
+
+
+    /*
+    *  METHOD       : Awake
+    *  DESCRIPTION  : Get a reference to the light component from the directional light object
+    *  PARAMETER    : NA
+    *  RETURNS      : NA
+    */
     void Awake ()
     {
         m_Light = GetComponent<Light>();
     }
 
-    //If environmental light estimation is enabled
+
+    /*
+    *  METHOD       : OnEnable
+    *  DESCRIPTION  : If environmental light estimation is enabled, start processing the incoming light data
+    *  PARAMETER    : NA
+    *  RETURNS      : NA
+    */
     void OnEnable()
     {
         //Fire the light frame event when available
@@ -69,7 +101,13 @@ public class LightEstimation : MonoBehaviour
         }
     }
 
-    //If environmental light estimation is disabled
+
+    /*
+    *  METHOD       : OnDisable
+    *  DESCRIPTION  : If environmental light estimation is disabled, stop processing the incoming light data
+    *  PARAMETER    : NA
+    *  RETURNS      : NA
+    */
     void OnDisable()
     {
         //De-register the lighting update event
@@ -80,6 +118,13 @@ public class LightEstimation : MonoBehaviour
     }
 
 
+    /*
+    *  METHOD       : FrameChanged
+    *  DESCRIPTION  : If light estimation is enabled, and set to environmental HDR, use the incoming light data to adjust the 
+    *                 AR object shadows, and surface reflections to align with the real world lighting and color
+    *  PARAMETER    : ARCameraFrameEventArgs : Incoming light data captured for current frame
+    *  RETURNS      : NA
+    */
     void FrameChanged(ARCameraFrameEventArgs args)
     {
         if (args.lightEstimation.mainLightDirection.HasValue)
@@ -108,4 +153,5 @@ public class LightEstimation : MonoBehaviour
         //ARCore returns color in HDR format (can be represented as FP16 and have values above 1.0)
         var camera = m_CameraManager.GetComponentInParent<Camera>();
     }
-}
+
+}//class
