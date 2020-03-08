0d9007a8b517ba1fce825714f084b09d1fd7b280
lefebvrerandy <Rlefebvre2256@conestogac.on.ca>
2020-03-08 16:56:47 -0400

    .

--- Assets/Scripts/PlacementWithDraggingDroppingController.cs
+++ Assets/Scripts/PlacementWithDraggingDroppingController.cs
@@ -101,7 +101,7 @@ public class PlacementWithDraggingDroppingController : MonoBehaviour
                 onTouchHold = false;
             }
         }
-        //if (true)
+        
         if (arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon) && Input.touchCount > 0)
         {
             Pose hitPose = hits[0].pose;
