ea8e2c697777816f8dce0573ebde334d2986ee18
DigitalCitizen110625 <bkarner5307@conestogac.on.ca>
2020-03-09 19:48:00 -0400

    Merging

---
+++ Assets/DeleteObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteObject : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject ObjectToDestroy;
+
+    public void DestroyGameObject()
+    {
+        if(ObjectToDestroy == null)
+        {
+            throw new System.ArgumentNullException("Object to destroy is null");
+        }
+
+        Destroy(ObjectToDestroy);
+    }
+}
