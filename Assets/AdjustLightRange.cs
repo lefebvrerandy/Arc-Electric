8470875232ef125028beb8d95c1043167b8800e3
DigitalCitizen110625 <bkarner5307@conestogac.on.ca>
2020-03-09 11:56:52 -0400

    Merging

---
+++ Assets/AdjustLightRange.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustLightRange : MonoBehaviour
+{
+    public void AdjustRange (float value)
+    {
+        Debug.Log(value);
+    }
+}
